12d84435ab58942ad63e9a5955316018f6104ddf
Cheryl Wang <huiyi.wang@mail.mcgill.ca>
2024-09-09 10:26:04 -0400

    solving GPU usage issue

--- Train_reach_1h.py
+++ Train_reach_1h.py
@@ -177,14 +177,17 @@ def main():
     env = SubprocVecEnv([make_env(env_name, i, seed=args.seed) for i in range(num_cpu)])
     env.render_mode = 'rgb_array'
     envs = VecVideoRecorder(env, "videos/" + env_name + '/training_log' ,
+        record_video_trigger=lambda x: x % 30000 == 0.3333, video_length=300)
+
+
+    ## EVAL
+    eval_env = SubprocVecEnv([make_env(env_name, i, seed=args.seed, eval_mode=True) for i in range(1)])
+    eval_env.render_mode = 'rgb_array'
+    eval_envs = VecVideoRecorder(eval_env, "videos/" + env_name + '/training_log' ,
         record_video_trigger=lambda x: x % 30000 == 0, video_length=300)
 
-    detect_color = 'green'
-    #envs.set_attr('set_color', detect_color)
-    envs.color = detect_color
-
     log_path = './Reach_Target_vel/policy_best_model/' + env_name + '/' + time_now + '/'
-    eval_callback = EvalCallback(envs, best_model_save_path=log_path, log_path=log_path, eval_freq=10000, deterministic=True, render=False)
+    eval_callback = EvalCallback(eval_envs, best_model_save_path=log_path, log_path=log_path, eval_freq=10000, n_eval_episodes=20, deterministic=True, render=False)
     
     print('Begin training')
     print(time_now)
--- Train_reach_3.py
+++ Train_reach_3.py
@@ -179,20 +179,12 @@ def main():
     envs = VecVideoRecorder(env, "videos/" + env_name + '/training_log' ,
         record_video_trigger=lambda x: x % 30000 == 0, video_length=300)
 
-    detect_color = 'green'
-    #envs.set_attr('set_color', detect_color)
-    envs.color = detect_color
-    
     
     ## EVAL
-    eval_env = SubprocVecEnv([make_env(env_name, i, seed=args.seed, eval_mode=True) for i in range(num_cpu)])
+    eval_env = SubprocVecEnv([make_env(env_name, i, seed=args.seed, eval_mode=True) for i in range(1)])
     eval_env.render_mode = 'rgb_array'
     eval_envs = VecVideoRecorder(eval_env, "videos/" + env_name + '/training_log' ,
         record_video_trigger=lambda x: x % 30000 == 0, video_length=300)
-
-    detect_color = 'green'
-    #envs.set_attr('set_color', detect_color)
-    eval_envs.color = detect_color
 
     log_path = './Reach_Target_vel/policy_best_model/' + env_name + '/' + time_now + '/'
     eval_callback = EvalCallback(envs, best_model_save_path=log_path, log_path=log_path, eval_freq=10000, deterministic=True, render=False)
