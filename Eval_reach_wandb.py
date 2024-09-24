bdc15e3c7aa3dbd3d2adde0b19a992a58be8ee2e
Cheryl Wang <huiyi.wang@mail.mcgill.ca>
2024-09-24 13:02:11 -0400

    evaluation script

--- Eval_reach_wandb.py
+++ Eval_reach_wandb.py
@@ -10,19 +10,23 @@ import random
 from tqdm.auto import tqdm
 import torch
 import mujoco
+import warnings
+
+# Ignore specific warning
+warnings.filterwarnings("ignore", message=".*tostring.*is deprecated.*")
 
 #obj2mjcf --obj-dir . --obj-filter beaker --save-mjcf --compile-model --decompose --overwrite --coacd-args.max-convex-hull 15
 
-model_num = '2024_09_13_12_56_365' #'2024_08_10_19_05_524' #'2024_06_22_19_48_33'
-env_name = "UR10eReach4C-v1"
+model_num = '2024_09_20_10_35_557' #'2024_08_10_19_05_524' #'2024_06_22_19_48_33'
+env_name = "UR10eEvalReach7C-v0"
 env = gym.make(f'mj_envs.robohive.envs:{env_name}')
 
-movie = True
+movie = False
 frame_width = 224
 frame_height = 224
 #cap = cv.VideoCapture(0)
 
-model = PPO.load('./Reach_Target_vel/policy_best_model/' + "UR10eReach4C-v1" +'/' + model_num + r'/best_model')
+model = PPO.load('./Reach_Target_vel/policy_best_model/' + "UR10eReach7C-v1" +'/' + model_num + r'/best_model')
 #model = PPO.load('./models/'+ model_num + r'/model')
 
 
@@ -33,7 +37,7 @@ detect_color = 'green'
 
 env.reset()
 
-trial = 3
+trial = 100
 success = 0
 
 frames = []
@@ -46,14 +50,14 @@ for i in tqdm(range(trial)):
     obs = env.reset()
     step = 0
     #ret, frame = cap.read()
-    while not done and step < 200:
+    while not solved and step < 400:
           #obs = env.obsdict2obsvec(env.obs_dict, env.obs_keys)[1]
           #obs = env.get_obs_dict()        
           action, _ = model.predict(obs, deterministic=False)
           #print(action)
           obs, reward, done, info = env.step(action)
           solved = info['solved']
-          if i < 3:
+          if i < trial:
               frame_n = env.rgb_out
               mask = env.mask_out
               frame_n = np.rot90(np.rot90(frame_n))
@@ -71,6 +75,6 @@ print(f"Success rate: {success/trial}")
 
 
 if movie:
-    os.makedirs('./videos1' +'/' + env_name, exist_ok=True)
-    skvideo.io.vwrite('./videos1'  +'/' + env_name + '/' + model_num + f'{view}_video.mp4', np.asarray(frames), inputdict = {'-r':'50'} , outputdict={"-pix_fmt": "yuv420p"})
-    skvideo.io.vwrite('./videos1'  +'/' + env_name + '/' + model_num + f'{view}_mask_video.mp4', np.asarray(frames_mask), inputdict = {'-r':'50'} , outputdict={"-pix_fmt": "yuv420p"})
+    os.makedirs('./videos' +'/' + env_name, exist_ok=True)
+    skvideo.io.vwrite('./videos'  +'/' + env_name + '/' + model_num + f'{view}_video.mp4', np.asarray(frames), inputdict = {'-r':'50'} , outputdict={"-pix_fmt": "yuv420p"})
+    skvideo.io.vwrite('./videos'  +'/' + env_name + '/' + model_num + f'{view}_mask_video.mp4', np.asarray(frames_mask), inputdict = {'-r':'50'} , outputdict={"-pix_fmt": "yuv420p"})
